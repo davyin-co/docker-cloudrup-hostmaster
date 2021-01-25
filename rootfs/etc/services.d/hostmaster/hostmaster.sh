144100b0c15fcc9aac47bbdb5131c8e26bdd7f91
terry zhang <zterry@qq.com>
2021-01-25 10:06:25 +0000

    update

--- rootfs/etc/services.d/hostmaster/hostmaster.sh
+++ rootfs/etc/services.d/hostmaster/hostmaster.sh
@@ -2,7 +2,7 @@
 ## https://github.com/ipumpkin/cloudrup/blob/master/docker-entrypoint.sh
 
 _drush_aegir() {
-    gosu aegir drush $@
+    drush $@
 }
 if [ -z $HOSTNAME ];then
 	HOSTNAME=`hostname --fqdn`
@@ -44,7 +44,7 @@ if [ ${PIPESTATUS[0]} == 0 ]; then
     echo "ÆGIR | Provision Commands found."
 else
     echo "ÆGIR | Provision Commands not found! Installing..."
-    gosu aegir drush dl provision-$PROVISION_VERSION --destination=/var/aegir/.drush/commands -y
+    _drush_aegir drush dl provision-$PROVISION_VERSION --destination=/var/aegir/.drush/commands -y
 fi
 
 # Returns true once mysql can connect.
@@ -62,7 +62,7 @@ done
 echo "ÆGIR | Database active! Checking for Hostmaster Install..."
 
 # Check if @hostmaster is already set and accessible.
-_drush_aegir @hostmaster vget site_name > /dev/null 2>&1
+_drush_aegir @hostmaster vget cron_key > /dev/null 2>&1
 if [ ${PIPESTATUS[0]} == 0 ]; then
   echo "ÆGIR | Hostmaster site found... Checking for upgrade platform..."
 
@@ -86,7 +86,7 @@ else
   echo "ÆGIR | -------------------------"
   echo "ÆGIR | Running: drush hostmaster-install"
 
-  set -ex
+  #set -ex
   _drush_aegir hostmaster-install -y --strict=0 $HOSTNAME \
     --aegir_db_host=$AEGIR_DATABASE_SERVER \
     --aegir_db_pass=$MYSQL_ROOT_PASSWORD \
@@ -95,7 +95,6 @@ else
     --aegir_db_grant_all_hosts=1 \
     --aegir_host=$HOSTNAME \
     --client_name=$AEGIR_CLIENT_NAME \
-    --client_email=$AEGIR_CLIENT_EMAIL \
     --makefile=$AEGIR_MAKEFILE \
     --profile=$AEGIR_PROFILE \
     --root=$AEGIR_HOSTMASTER_ROOT \
@@ -104,35 +103,28 @@ fi
 
 sleep 3
 
-
-# Exit on the first failed line.
-set -e
-
-echo "ÆGIR | Running 'drush cc drush' ... "
-_drush_aegir cc drush
-
-echo "ÆGIR | Enabling hosting queued..."
-_drush_aegir @hostmaster en hosting_queued -y
-
 if [ ! -f /var/aegir/.drush ]; then
     mkdir -p /var/aegir/.drush
-    chown -R aegir:aegir /var/aegir/.drush
 fi
 if [ ! -f /var/aegir/config ]; then
     mkdir -p /var/aegir/config
-    chown -R aegir:aegir /var/aegir/config
 fi
+chown -R aegir:www-data /var/aegir/cloudrup
 
 ls -lah /var/aegir
-
-# We need a ULI here because aegir only outputs one on install, not on subsequent verify.
-echo "ÆGIR | Getting a new login link ... "
-_drush_aegir @hostmaster uli
 
 echo "ÆGIR | Clear Hostmaster caches ... "
 _drush_aegir cc drush
 _drush_aegir @hostmaster cc all
 
-echo "ÆGIR | Clear Hosting queues locks ... "
-_drush_aegir @hostmaster sqlq "delete from semaphore where name like 'hosting_queue_%_running'"
+# We need a ULI here because aegir only outputs one on install, not on subsequent verify.
+echo "ÆGIR | Getting a new login link ... "
+_drush_aegir @hostmaster uli
 
+echo "ÆGIR | Enabling hosting queued..."
+_drush_aegir @hostmaster en hosting_queued -y
+
+echo "ÆGIR | Clear Hosting queues locks ... "
+
+#_drush_aegir @hostmaster sqlq "delete from semaphore where name like 'hosting_queue_%_running'"
+#_drush_aegir @hostmaster sqlq 'truncate semaphore' -vvv
